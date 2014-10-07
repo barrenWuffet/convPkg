bd09177ee74fce82f0ebd0464295dc3ab8a4e894
barrenWuffet <sargow@BSTNAPL20871.onefirm.corp>
2014-10-07 13:39:48 -0400

    Recopied code as prior version wasn't working.

--- R/lsos.R
+++ R/lsos.R
@@ -9,7 +9,8 @@
 #' @examples
 #' lsos()
 ###
-ls.objects <- function (pos = 1, pattern, order.by,
+# improved list of objects
+.ls.objects <- function (pos = 1, pattern, order.by,
                         decreasing=FALSE, head=FALSE, n=5) {
     napply <- function(names, fn) sapply(names, function(x)
                                          fn(get(x, pos = pos)))
@@ -18,23 +19,20 @@ ls.objects <- function (pos = 1, pattern, order.by,
     obj.mode <- napply(names, mode)
     obj.type <- ifelse(is.na(obj.class), obj.mode, obj.class)
     obj.size <- napply(names, object.size)
-    obj.prettysize <- sapply(obj.size, function(r) prettyNum(r, big.mark = ",") )
     obj.dim <- t(napply(names, function(x)
                         as.numeric(dim(x))[1:2]))
     vec <- is.na(obj.dim)[, 1] & (obj.type != "function")
     obj.dim[vec, 1] <- napply(names, length)[vec]
-    out <- data.frame(obj.type, obj.size,obj.prettysize, obj.dim)
-    names(out) <- c("Type", "Size", "PrettySize", "Rows", "Columns")
+    out <- data.frame(obj.type, obj.size, obj.dim)
+    names(out) <- c("Type", "Size", "Rows", "Columns")
     if (!missing(order.by))
         out <- out[order(out[[order.by]], decreasing=decreasing), ]
-        out <- out[c("Type", "PrettySize", "Rows", "Columns")]
-        names(out) <- c("Type", "Size", "Rows", "Columns")
     if (head)
         out <- head(out, n)
     out
 }
-
+# shorthand
 lsos <- function(..., n=10) {
-    ls.objects(..., order.by="Size", decreasing=TRUE, head=TRUE, n=n)
+    .ls.objects(..., order.by="Size", decreasing=TRUE, head=TRUE, n=n)
 }
 
