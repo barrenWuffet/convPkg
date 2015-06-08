7d9ab6d81c81432eab353b894c45fb48a0fdc2e0
Zach Mayer <zach.mayer@gmail.com>
2015-06-08 15:21:09 -0400

    try to hit 100% test coverage

--- tests/testthat/test-clusDropVar2.R
+++ tests/testthat/test-clusDropVar2.R
@@ -4,6 +4,6 @@ context("clusDropVar2")
 ## TODO: Add more tests
 
 test_that("clusDropVar2 works", {
-  d <- clusDropVar(iris, 3:5, threshold = .6, vte = "Species")
+  d <- clusDropVar(iris, 3:5, threshold = .6, vte = "Species", xLoopsBeforePrint=1)
   expect_is(d, "list")
 })
