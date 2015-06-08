0b8d4d76bab8a65eeccab18c01dd836628ec262d
Zach Mayer <zach.mayer@gmail.com>
2015-06-08 14:33:33 -0400

    documentation cleanup

--- R/cnn.R
+++ R/cnn.R
@@ -1,7 +1,7 @@
 #' A Function to print the column number, name and variable type for each column in a data.frame.
 #'
 #' This function prints the column number, name, and variable type for each column in a data.frame.
-#' @param x A data.frame object
+#' @param dataset A data.frame object
 #' @keywords names class
 #' @export
 #' @examples
--- R/lsos.R
+++ R/lsos.R
@@ -3,10 +3,12 @@
 #' http://stackoverflow.com/questions/1358003/tricks-to-manage-the-available-memory-in-an-r-session
 #'
 
-
+#' @param ... Passed to .ls.objects
+#' @param n Number of objects to show
 #' @keywords memory
 #' @export
 #' @examples
+#' data(iris)
 #' lsos()
 ###
 # improved list of objects
@@ -15,4 +17,3 @@
 lsos <- function(..., n=10) {
     .ls.objects(..., order.by="Size", decreasing=TRUE, head=TRUE, n=n)
 }
-
