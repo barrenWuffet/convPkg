7c12e7f2df7f667185e94bd9668a9e3a9620592f
Sam Argow <sargow@vs-aws-rhead90.aait.local>
2018-09-11 14:06:44 -0400

    updated documentation

--- R/cat_names.R
+++ R/cat_names.R
@@ -1,5 +1,9 @@
 #' A function to print variable names for easy pasting to a new character vector.
-
+#'
+#' @param x a data.frame
+#' @examples
+#' cat_names(iris)
+#'
 
 cat_names <- function(x){
   cat(paste(",'", names(x), "'", "\n", sep = ""))
