815a5eb77f3bed4f77f6130e138bb45f7959fefe
Sam Argow <sargow@vs-aws-rhead90.aait.local>
2018-09-11 14:06:25 -0400

    added new function

---
+++ R/convert_date.R
@@ -0,0 +1,30 @@
+#' Converts all columns of class POSIXct or POSIXt in a data.frame to Date class.
+#'
+#' @param xx A data.frame containing columns of class POSIXct or POSIXt
+#'
+#' @return data.frame with any columns of class POSIXct or POSIXt converted to Dates
+#' @export
+#'
+#' @examples df2 <- convert_date(df1)
+#'
+#'
+#' @author \itemize{
+#'    \item Andrei Rukavina - \url{https://github.com/arukavina}
+#'    \item Thijn van der Heijden - \email{avanderheijden@@alixpartners.com}
+#'    \item Zach Armentrout - \email{zarmentrout@alixpartners.com}
+#'    \item Qianbo Wang - \email{qwang@alixpartners.com}
+#'    \item James Wang - \email{swang@alixpartners.com}
+#' }
+#'
+#'
+#'
+convert_date <- function(xx){
+
+  dateind <- names(which(sapply(sapply(xx, class),function(x) any(x %in% c("POSIXct", "POSIXt" )))))
+  cat('found ',length(dateind), ' dates : \n' )
+  lapply(dateind,function(x) cat(x,' --- \n'))
+
+  xx[,dateind] <- data.frame(lapply(dateind,function(x) as.Date(xx[,x])))
+
+  return(xx)
+}
